8ea19ecabe8b6a7a66b0b760779fe56c0c759fa1
Nam Phạm <44641118+NamPhamc99@users.noreply.github.com>
2020-10-04 17:34:10 +0700

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'DoITScholarFrontend',
+      name: 'doit_scholar_frontend',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
